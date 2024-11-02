7a1b128d6bd5f4eb4d3597d8fc8cc2e4a172601c
Eduardo Silva <eduardosilva@disroot.org>
2024-11-02 16:03:34 +0000

    Got the constants from the CPP SDK and will comb trough them later

--- constants.go
+++ constants.go
@@ -115,3 +115,138 @@ const (
 const (
 // CamNose
 )
+
+
+// Some other constants that I need to get trough
+// // bit fields
+// enum irsdk_EngineWarnings 
+// {
+// 	irsdk_waterTempWarning		= 0x01,
+// 	irsdk_fuelPressureWarning	= 0x02,
+// 	irsdk_oilPressureWarning	= 0x04,
+// 	irsdk_engineStalled			= 0x08,
+// 	irsdk_pitSpeedLimiter		= 0x10,
+// 	irsdk_revLimiterActive		= 0x20,
+// };
+//
+// // global flags
+// enum irsdk_Flags
+// {
+// 	// global flags
+// 	irsdk_checkered				= 0x00000001,
+// 	irsdk_white					= 0x00000002,
+// 	irsdk_green					= 0x00000004,
+// 	irsdk_yellow				= 0x00000008,
+// 	irsdk_red					= 0x00000010,
+// 	irsdk_blue					= 0x00000020,
+// 	irsdk_debris				= 0x00000040,
+// 	irsdk_crossed				= 0x00000080,
+// 	irsdk_yellowWaving			= 0x00000100,
+// 	irsdk_oneLapToGreen			= 0x00000200,
+// 	irsdk_greenHeld				= 0x00000400,
+// 	irsdk_tenToGo				= 0x00000800,
+// 	irsdk_fiveToGo				= 0x00001000,
+// 	irsdk_randomWaving			= 0x00002000,
+// 	irsdk_caution				= 0x00004000,
+// 	irsdk_cautionWaving			= 0x00008000,
+//
+// 	// drivers black flags
+// 	irsdk_black					= 0x00010000,
+// 	irsdk_disqualify			= 0x00020000,
+// 	irsdk_servicible			= 0x00040000, // car is allowed service (not a flag)
+// 	irsdk_furled				= 0x00080000,
+// 	irsdk_repair				= 0x00100000,
+//
+// 	// start lights
+// 	irsdk_startHidden			= 0x10000000,
+// 	irsdk_startReady			= 0x20000000,
+// 	irsdk_startSet				= 0x40000000,
+// 	irsdk_startGo				= 0x80000000,
+// };
+//
+//
+// // status 
+// enum irsdk_TrkLoc
+// {
+// 	irsdk_NotInWorld = -1,
+// 	irsdk_OffTrack,
+// 	irsdk_InPitStall,
+// 	irsdk_AproachingPits,
+// 	irsdk_OnTrack
+// };
+//
+// enum irsdk_TrkSurf
+// {
+// 	irsdk_SurfaceNotInWorld = -1,
+// 	irsdk_UndefinedMaterial = 0,
+//
+// 	irsdk_Asphalt1Material,
+// 	irsdk_Asphalt2Material,
+// 	irsdk_Asphalt3Material,
+// 	irsdk_Asphalt4Material,
+// 	irsdk_Concrete1Material,
+// 	irsdk_Concrete2Material,
+// 	irsdk_RacingDirt1Material,
+// 	irsdk_RacingDirt2Material,
+// 	irsdk_Paint1Material,
+// 	irsdk_Paint2Material,
+// 	irsdk_Rumble1Material,
+// 	irsdk_Rumble2Material,
+// 	irsdk_Rumble3Material,
+// 	irsdk_Rumble4Material,
+//
+// 	irsdk_Grass1Material,
+// 	irsdk_Grass2Material,
+// 	irsdk_Grass3Material,
+// 	irsdk_Grass4Material,
+// 	irsdk_Dirt1Material,
+// 	irsdk_Dirt2Material,
+// 	irsdk_Dirt3Material,
+// 	irsdk_Dirt4Material,
+// 	irsdk_SandMaterial,
+// 	irsdk_Gravel1Material,
+// 	irsdk_Gravel2Material,
+// 	irsdk_GrasscreteMaterial,
+// 	irsdk_AstroturfMaterial,
+// };
+//
+// enum irsdk_SessionState
+// {
+// 	irsdk_StateInvalid,
+// 	irsdk_StateGetInCar,
+// 	irsdk_StateWarmup,
+// 	irsdk_StateParadeLaps,
+// 	irsdk_StateRacing,
+// 	irsdk_StateCheckered,
+// 	irsdk_StateCoolDown
+// };
+//
+// enum irsdk_CameraState
+// {
+// 	irsdk_IsSessionScreen          = 0x0001, // the camera tool can only be activated if viewing the session screen (out of car)
+// 	irsdk_IsScenicActive           = 0x0002, // the scenic camera is active (no focus car)
+//
+// 	//these can be changed with a broadcast message
+// 	irsdk_CamToolActive            = 0x0004,
+// 	irsdk_UIHidden                 = 0x0008,
+// 	irsdk_UseAutoShotSelection     = 0x0010,
+// 	irsdk_UseTemporaryEdits        = 0x0020,
+// 	irsdk_UseKeyAcceleration       = 0x0040,
+// 	irsdk_UseKey10xAcceleration    = 0x0080,
+// 	irsdk_UseMouseAimMode          = 0x0100
+// };
+//
+// enum irsdk_PitSvFlags
+// {
+// 	irsdk_LFTireChange		= 0x0001,
+// 	irsdk_RFTireChange		= 0x0002,
+// 	irsdk_LRTireChange		= 0x0004,
+// 	irsdk_RRTireChange		= 0x0008,
+//
+// 	irsdk_FuelFill			= 0x0010,
+// 	irsdk_WindshieldTearoff	= 0x0020,
+// 	irsdk_FastRepair		= 0x0040
+// };
+
+//----
+//
